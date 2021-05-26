cb327214b397f7a4f66f5febbce24edd58cda754
Jack M <jack@matthews-net.org.uk>
2021-05-26 06:42:28 +0000

    new custom role position calculator this should fix roles ending up at the bottom, and retroactively fix ones that are already stuck at the bottom also allows custom roles to be under transparent roles

--- cogs/role_common.py
+++ cogs/role_common.py
@@ -5,7 +5,7 @@ from sqlalchemy.future import select
 from sqlalchemy.orm import sessionmaker
 
 from .utils.models import Booster
-from .utils.errors import BelowMember, TooManyRoles
+from .utils.errors import TooManyRoles
 
 
 class RoleCommon(commands.Cog):
@@ -19,21 +19,38 @@ class RoleCommon(commands.Cog):
         # get a member object of our bot user
         bot = guild.get_member(self.bot.user.id)
         bot_top_role = bot.roles[-1]  # bot's top role
-        member_top_role = member.roles[-1]  # member's top role
 
-        # failsafes
-        if bot_top_role <= member_top_role:
-            raise BelowMember
-        elif len(guild.roles) >= 249:
+        if len(guild.roles) >= 249:
             raise TooManyRoles
 
         async with self.sessionmaker() as session:
             async with session.begin():
                 result = await session.execute(select(Booster).where(Booster.user_id == member.id, Booster.guild_id == guild.id))
                 booster = result.scalars().first()
-                role = guild.get_role(booster.role_id)
-                # create a new role if we can't just get one
-                if role == None:
+                custom_role = guild.get_role(booster.role_id)
+
+                visible_role = None
+
+                # find the role that currently is showing for color
+                for member_role in reversed(member.roles):
+                    if member_role.color != Color.default():
+                        visible_role = member_role
+                        break
+                
+                # if there isn't a colored role just make the @everyone role it so that the next 2nd bit works
+                if visible_role == None:
+                    visible_role = guild.default_role
+                
+                # return the role if the role is already in the right spot
+                if custom_role != None:
+                    if visible_role == custom_role:
+                        return custom_role
+
+                # check to make sure the bot can make roles above the visible role
+                if bot_top_role <= visible_role:
+                    return # TODO: replace with a exception
+                
+                if custom_role == None:
                     # usernames cant be longer than 30 charachers, even with the addition of discriminator and "'s Custom Role" we're barely above 50
                     role_name = f"{str(member)}'s Custom Role"
                     # use the boosters custom role name if set
@@ -45,22 +62,10 @@ class RoleCommon(commands.Cog):
                         role_color = booster.role_color
 
                     custom_role = await guild.create_role(name=role_name, color=role_color, reason=f"Creating custom role for {str(member)}")
-                    # put the role at the right spot
-                    # if the bot is only just above the member, we have to shuffle more roles around
-                    if member_top_role.position + 1 == bot_top_role.position:
-                        new_positions = {
-                            custom_role: member_top_role.position,
-                            member_top_role: member_top_role.position-1
-                        }
-                        await guild.edit_role_positions(positions=new_positions, reason=f"Moving roles around to get {custom_role.name}'s color to show")
-
-                    elif member_top_role.position+1 < bot_top_role.position:  # bot role is above member, we should be good
-                        await custom_role.edit(position=member_top_role.position+1)
-
-                    # assign the role to the user
-                    await member.add_roles(custom_role)
-                    # update booster role id
                     booster.role_id = custom_role.id
+                
+                await custom_role.edit(position=visible_role.position+1)
+                
             await session.commit()
 
     async def assure_booster(self, member):
